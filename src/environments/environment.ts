62df64e814b0f8b994c7782741e7c4067b4163e2
Ivan Ivanov <ivanivamar99@gmail.com>
2023-04-08 19:24:20 +0200

    more

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,17 @@
-export const environment = {
-    firebase: {
-        projectId: 'my-youtube-redesign',
-        appId: '1:579470838542:web:53e1d3bbecb33f2cc6655f',
-        storageBucket: 'my-youtube-redesign.appspot.com',
-        apiKey: 'AIzaSyDU2giYC7r-V9GdlCrWwH9wA4szieDwFOE',
-        authDomain: 'my-youtube-redesign.firebaseapp.com',
-        messagingSenderId: '579470838542',
-    },
-}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDU2giYC7r-V9GdlCrWwH9wA4szieDwFOE",
+    authDomain: "my--redesign.firebaseapp.com",
+    projectId: "my-youtube-redesign",
+    storageBucket: "my-youtube-redesign.appspot.com",
+    messagingSenderId: "579470838542",
+    appId: "1:579470838542:web:a70f80b1363e678dc6655f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
